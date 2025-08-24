f65ce8917982ec4d02d7b29bed66342fbc7c3a1a
Chester Wong <z5588909@ad.unsw.edu.au>
2025-08-24 18:41:37 +1000

    changes

--- first.py
+++ first.py
@@ -1,1 +1,9 @@
-print(1 + 1)
+import random
+from datetime import datetime
+
+seed = random.randint(1, 10)
+date = datetime.now()
+newdate = datetime.strftime(date, '%b %d, %Y')
+print(seed)
+print(date)
+print(newdate)
